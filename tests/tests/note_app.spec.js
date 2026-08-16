ef21f223c8e62f7bfc00c5378a69275117a467dc
reagan luyinda <luyindareagan@gmail.com>
2026-08-16 21:53:04 +0300

    ref; changed the test for the testboxes from getByRole to getByLabel which is less problematic

--- tests/tests/note_app.spec.js
+++ tests/tests/note_app.spec.js
@@ -12,8 +12,8 @@ test('front page can be opened', async ({page}) => {
 test('user can log in', async ({page}) => {
     await page.goto('http://localhost:5173');
     await page.getByRole('button', {name: 'login'}).click();
-    await page.getByRole('textbox').first().fill('Reagan');
-    await page.getByRole('textbox').last().fill('123456789');
+    await page.getByLabel('username').fill('Reagan');
+    await page.getByLabel('password').fill('123456789');
     await page.getByRole('button', {name: 'login'}).click();
 
     await expect(page.getByText('Welcome, Luyinda Reagan!')).toBeVisible();
